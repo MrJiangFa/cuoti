c01c6cd61e83efe4e0c4dce862048b8c158e487a
jiangfa <jiangfa@corp.netease.com>
2019-07-30 21:29:38 +0800

    新增优雅推出

--- src/io/nio/GracefulOut.java
+++ src/io/nio/GracefulOut.java
@@ -1,7 +1,25 @@
 package io.nio;
 
+import java.util.concurrent.TimeUnit;
+
 public class GracefulOut {
-    private void gracefulOut1(){
-        Runtime.getRuntime().
+    //优雅推出的方式之一：JVM睡了7秒，然后执行线程；
+    private static void gracefulOut1() throws InterruptedException {
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+        {
+            System.out.println("shutdownhook execute start..");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("shutdownhook execute end..");
+        }, ""));
+        TimeUnit.SECONDS.sleep(7);
+        System.exit(0);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        gracefulOut1();
     }
 }
