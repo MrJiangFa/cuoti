ca331ee7e8c290c9815fb461c907b00c5f2490cd
jiangfa <jiangfa@corp.netease.com>
2019-08-27 20:56:46 +0800

    字符串相关内容

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,33 @@
-import java.util.Hashtable;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.FutureTask;
 
 public class Test {
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{1,2},{3,4}};
-        System.out.println(arr[0][1]);
+//        Map<Integer, Integer> map = new HashMap<>();
+//        Map<Integer, Integer> linkedMap = new LinkedHashMap<>();
+//        for (int i = 0; i < 100; i++) {
+//            map.put(i, i);
+//            linkedMap.put(i, i);
+//        }
+//        map.forEach((k, v) -> System.out.println("map" + k + "=" + v));
+//        linkedMap.forEach((k, v) -> System.out.println("linkedhashmap" + k + "=" + v));
+//        map.remove(10);
+//        linkedMap.remove(10);
+//        map.put(10, 10);
+//        linkedMap.put(10, 10);
+//        System.out.println("=================================");
+//        map.forEach((k, v) -> System.out.println("after insert map" + k + "=" + v));
+//        linkedMap.forEach((k, v) -> System.out.println("after insert linkedhashmap" + k + "=" + v));
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+//        long userId = 56800004874L;
+//        System.out.println(String.valueOf(userId).hashCode());
+        System.out.println((char)3000);
+    }
+    private void test(int a){
 
+    }
+    private int test(){
+        return 0;
     }
 }
